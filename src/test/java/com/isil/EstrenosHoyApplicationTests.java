8f5a1d764c163a6427902fb27c5a074d6dd35022
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2021-05-03 22:23:26 -0500

    inicio

---
+++ src/test/java/com/isil/EstrenosHoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstrenosHoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
