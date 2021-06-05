9f791d889653b87383ab3a611cafba75eac5014c
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2021-06-05 12:14:40 -0500

    lista usuarios

--- src/main/java/com/isil/loader/PeliculaLoader.java
+++ src/main/java/com/isil/loader/PeliculaLoader.java
@@ -23,12 +23,10 @@ public class PeliculaLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<Pelicula> peliculas = new ArrayList<>();
-        peliculas.add(new Pelicula(null,"spiderman","1h",null,"español","eeuu",1,1,"tio","accion",1,"sisi",1,"aa",1,"thro","sisi",null,null,null));
-      //  peliculas.add(new Pelicula(null,"0000000001","ISIL SA",null));
-     //   peliculas.add(new Pelicula(null,"0000000002","Bodega Don Pepe SA",null));
-
-
-        peliculaRepository.saveAll(peliculas);
+        // List<Pelicula> peliculas = new ArrayList<>();
+        // peliculas.add(new Pelicula(null,"spiderman","1h",null,"español","eeuu",1,1,"tio","accion",1,"sisi",1,"aa",1,"thro","sisi",null,null,null));
+        // peliculas.add(new Pelicula(null,"0000000001","ISIL SA",null));
+        // peliculas.add(new Pelicula(null,"0000000002","Bodega Don Pepe SA",null));
+        // peliculaRepository.saveAll(peliculas);
     }
 }
